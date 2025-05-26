0e7e3177a88794fc7e0dbb42a51021b55ef4e7c8
SanelisiweZ <sanelisiwepsz@gmail.com>
2025-05-26 21:16:53 +0200

    Vibes Hackathon

--- Farmers.js
+++ Farmers.js
@@ -1,1 +1,26 @@
+function signUpNow() {
+  let firstName = prompt("What is your First Name?");
+  let lastName = prompt("What is your last Name?");
+  let emailAddress = prompt("What is your email address?");
+  let age = prompt("How old are you?");
+  if (age >= 16) {
+    alert(
+      "Thank you " +
+        firstName +
+        " " +
+        lastName +
+        " for signing up for one of our Workshop😁! A confirmation email has been sent you. We will be in touch soon with further details"
+    );
+  } else {
+    alert(
+      "Sorry " +
+        firstName +
+        " " +
+        lastName +
+        ". You need to be 16 and above to qualify for this bootcamp☹️"
+    );
+  }
+}
 
+let signUpNowButton = document.querySelector("button");
+signUpNowButton.addEventListener("click", signUpNow);
